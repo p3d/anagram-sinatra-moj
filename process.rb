b155b0174a5c184096b8b5553e02a0126dfcf56b
Paul Dalton <paul@p3d.co.uk>
2014-11-19 10:19:49 +0000

    ignore single letter words in dictionary

--- process.rb
+++ process.rb
@@ -5,6 +5,9 @@ while (word = file.gets)
   unless word.nil?
     word.chomp!.downcase!
   end
+  if word.length == 1
+    next
+  end
   sorted_word = word.split(//).sort.join
   if lookup.has_key?(sorted_word)
     lookup[sorted_word] << word
